a1dfcea1206a74bf6b858ee345c7f181a76a3edf
darin <njdenkj@163.com>
2022-05-11 23:48:41 +0800

    11

---
+++ start/src/main/java/com/darin/start/StartApplication.java
@@ -0,0 +1,17 @@
+package com.darin.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author darin
+ * @Desc 启动类
+ */
+@SpringBootApplication
+public class StartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class, args);
+    }
+
+}
